266a711294318d7d86f63a7c50d175e09f658f4e
Alexander Walford <74402359+alexanderwalford-official@users.noreply.github.com>
2022-08-08 14:34:52 +0100

    Remove Whitespace

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,15 +1,10 @@
-// Calculator.cpp
-// Alexander Walford 2022
-
+// Alexander Walford 2022 - Compact, fast and simple calculator app. Could probably be optimised even further or more functionaility could be added.
 #include <iostream>
 #include <vector>
 #include <sstream>
 #include <string>
-
 using namespace std;
-
 int sum = 0;
-
 int main () {
     std :: string input;
     std :: cout << "Basic C++ Calculator" << std :: endl;
@@ -20,16 +15,12 @@ int main () {
     calculate(input);
     return 0;
 }
-
 int calculate (std :: string input) {
-
     std::stringstream nonsplit(input);
     std :: string segment;
     std :: vector<std :: string> seglist;
-
     // to set this, remove all numbers and keep special character index 0 pointer position only
     char chartype = rm_nums(input)[0];
-
     while(std :: getline(nonsplit, segment, chartype)) {
         seglist.push_back(segment);
     }
@@ -41,8 +32,7 @@ int calculate (std :: string input) {
         sum = (chartype == '*') ? b=b--:sum=sum*stoi(s);
         std :: cout << (b == 0) ? 0:err();
         std :: cout << chartype << s << std :: endl;
-    }
-    
+    }   
     std :: cout << "=" << sum << std :: endl;
     std :: cout << "" << std :: endl;
     std :: cout << "Press ENTER to continue.." << std :: endl;
@@ -50,7 +40,6 @@ int calculate (std :: string input) {
     main();
     return 0;
 }
-
 string rm_nums(string str)
 {
     int current = 0;
@@ -59,7 +48,6 @@ string rm_nums(string str)
     }
     return str.substr(0,current);
 }
-
 int err() {
     std :: cout << "ERROR: Did not find an operator.";
     std :: cin;
